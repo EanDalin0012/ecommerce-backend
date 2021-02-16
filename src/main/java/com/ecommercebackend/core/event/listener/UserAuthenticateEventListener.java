aaa2c1d2223871249f91bee2d5acebd0022daa78
Eanda <eandalined@gmail.com>
2021-02-16 23:06:51 +0700

    add new success login event and xml dao

--- src/main/java/com/ecommercebackend/core/event/listener/UserAuthenticateEventListener.java
+++ src/main/java/com/ecommercebackend/core/event/listener/UserAuthenticateEventListener.java
@@ -3,6 +3,7 @@ package com.ecommercebackend.core.event.listener;
 import com.ecommercebackend.core.dao.SuccessLoginDao;
 import com.ecommercebackend.core.event.UserAuthenticateEvent;
 import com.ecommercebackend.core.model.map.ModelMap;
+import com.ecommercebackend.core.util.SystemDateUtil;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,24 +22,25 @@ public class UserAuthenticateEventListener implements ApplicationListener<UserAu
     public void onApplicationEvent(UserAuthenticateEvent event) {
         try {
             ObjectMapper objectMapper = new ObjectMapper();
-            ModelMap d = (ModelMap) event.getSource();
-            String strData  = objectMapper.writeValueAsString(event.getSource());
-            ModelMap data =  objectMapper.convertValue(strData, ModelMap.class);
+            ModelMap data = (ModelMap) event.getSource();
             ModelMap device = data.getModelMap("deviceInfo");
             String userName = data.getString("userName");
             String networkIp = data.getString("networkIP");
 
             if (userName != null) {
+                String date = SystemDateUtil.getDateFormat("yyyy-MM-dd hh:mm:ss");
                 ModelMap input = new ModelMap();
                 input.setString("userAgent", device.getString("userAgent"));
+                input.setString("browser", device.getString("browser"));
                 input.setString("os", device.getString("os"));
                 input.setString("device", device.getString("device"));
-                input.setString("osVersion", device.getString("osVersion"));
-                input.setString("browserVersion", device.getString("browserVersion"));
+                input.setString("osVersion", device.getString("os_version"));
+                input.setString("browserVersion", device.getString("browser_version"));
                 input.setString("deviceType", device.getString("deviceType"));
                 input.setString("orientation", device.getString("orientation"));
                 input.setString("networkIP", networkIp);
                 input.setString("userName", userName);
+                input.setString("date", date);
 
                 log.info("User Login Success Info "+objectMapper.writeValueAsString(input));
                 successLoginDao.save(input);
